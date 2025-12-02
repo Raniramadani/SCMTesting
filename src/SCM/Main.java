f69c2606026960412d5e6f93cf952d093d4889fe
aulia <auliaramadaniar08@gmail.com>
2025-12-03 00:23:16 +0800

    Menambahkan output baru di branch fitur

--- src/SCM/Main.java
+++ src/SCM/Main.java
@@ -4,6 +4,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello from SCM Test!");
         System.out.println("Testing SCM - Perubahan 1");
+        System.out.println("Output dari branch fitur-tambah-output");
 
     }
 }
