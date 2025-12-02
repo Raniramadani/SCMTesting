c18ddcc26f6c6447c93c44c4446275d012dfcdd9
aulia <auliaramadaniar08@gmail.com>
2025-12-03 01:00:38 +0800

    Perubahan di branch fitur

--- src/SCM/Main.java
+++ src/SCM/Main.java
@@ -2,9 +2,7 @@
 package SCM;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello from SCM Test!");
-        System.out.println("Testing SCM - Perubahan 1");
-        System.out.println("Output dari branch fitur-tambah-output");
+        System.out.println("Ini perubahan dari branch fitur");
 
     }
 }
